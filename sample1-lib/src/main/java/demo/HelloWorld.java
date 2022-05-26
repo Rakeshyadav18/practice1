950b1e0ddca018ba702799a5109c615ff5d47740
sumitps09081997 <106057014+sumitps09081997@users.noreply.github.com>
2022-05-26 16:35:35 +0530

    sumitupdate

--- sample1-lib/src/main/java/demo/HelloWorld.java
+++ sample1-lib/src/main/java/demo/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	System.out.println("its rakesh guys");
 	System.out.println("its rajesh guys");
-	
+	System.err.println("its me sumit");
 
 	}
 
