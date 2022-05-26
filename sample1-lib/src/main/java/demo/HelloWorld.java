7f23a5eb1398aec5ab3f964b203eebed2ab474c3
sumitps09081997 <106057014+sumitps09081997@users.noreply.github.com>
2022-05-26 16:40:22 +0530

    sumit update1

--- sample1-lib/src/main/java/demo/HelloWorld.java
+++ sample1-lib/src/main/java/demo/HelloWorld.java
@@ -5,13 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	System.out.println("its rakesh guys");
 	System.out.println("its rajesh guys");
-<<<<<<< HEAD
+
 	System.err.println("its me sumit");
-=======
+
 	System.out.println("It's shashi");
-	System.out.println("Its Koushal");
 	
->>>>>>> 1bb392f9d531f9597dd3949729002bd94ab6ebdb
+	
 
 	}
 
